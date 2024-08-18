8bbe42c8a122e46c1c46930e8de7390000ebda97
Nathan <ospinonathan@gmail.com>
2024-08-17 21:26:13 -0500

    fix(images): :bug: Actualización de la logica de traer los repositorios por id

--- src/controllers/repository.controller.ts
+++ src/controllers/repository.controller.ts
@@ -1,59 +1,61 @@
 import { Request, Response } from "express";
-import { deleteRepository, postRepository, putRepository } from "../services/repository.service";
+import { deleteRepository, getRepositories, postRepository, putRepository } from "../services/repository.service";
 
 export const postRepositoryController = async (req: Request, res: Response) => {
-    try {
-        const repository = req.body;
-        const files = req.files as Express.Multer.File[];
+  try {
+    const repository = req.body;
+    const files = req.files as Express.Multer.File[];
 
-        const response = await postRepository(repository, files);
-        res.status(201).json(response);
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: "Error registering repository.",
-        });
-    }
+    const response = await postRepository(repository, files);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Error registering repository.",
+    });
+  }
 };
 
-
 export const getRepositoriesController = async (req: Request, res: Response) => {
-    // try {
-    //     const userId = req.params.userId;
-    //     const response = await listRepositories(userId);
-    //     res.status(200).json(response);
-    // } catch (error) {
-    //     res.status(500).json({
-    //         success: false,
-    //         message: "Error retrieving repositories.",
-    //     });
-    // }
+  try {
+    const userId = req.params.userId;
+    const response = await getRepositories(userId);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error retrieving repositories.",
+    });
+  }
 };
 
 export const putRepositoryController = async (req: Request, res: Response) => {
-    try {
-        const repository = req.body;
-        const files = req.files as Express.Multer.File[];
+  try {
+    const repository = req.body;
+    const files = req.files as Express.Multer.File[];
 
-        const response = await putRepository(repository, files);
-        res.status(201).json(response);
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: "Error updating repository.",
-        });
-    }
+    const response = await putRepository(repository, files);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Error updating repository.",
+    });
+  }
 };
 
 export const deleteRepositoryController = async (req: Request, res: Response) => {
-    try {
-        const repository = req.body;
-        const response = await deleteRepository(repository);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Error deleting repository.",
-        });
-    }
+  try {
+    const repository = req.body;
+    const response = await deleteRepository(repository);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error deleting repository.",
+    });
+  }
 };
+function listRepositories(userId: string) {
+  throw new Error("Function not implemented.");
+}
